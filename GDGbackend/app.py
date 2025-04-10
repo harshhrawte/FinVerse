8d0f21dbbe4543fe85a6a9d862949c4c21910d18
JayantDethe26 <dethejayant@gmail.com>
2025-04-10 12:49:34 +0530

    port changes

--- GDGbackend/app.py
+++ GDGbackend/app.py
@@ -224,5 +224,5 @@ def get_dynamic_quiz():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("FLASK_PORT", 5000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
